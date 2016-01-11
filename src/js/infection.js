2041cd0c0bbc84cfff27ce5a815266e31cea18a9
Bernardo Sulzbach <mafagafogigante@gmail.com>
2016-01-11 15:18:26 -0200

    Improved the game script

--- src/js/infection.js
+++ src/js/infection.js
@@ -1,7 +1,7 @@
 function colorBasedOnValue(value) {
     var normalizedValue = Math.floor(value * 255);
     var redHexadecimal = (normalizedValue).toString(16);
-    return "#" + redHexadecimal + "0000";
+    return '#' + redHexadecimal + '0000';
 }
 
 window.onload = function () {
@@ -9,16 +9,21 @@ window.onload = function () {
     var context = canvas.getContext('2d');
     var width = canvas.width;
     var height = canvas.height;
+    var squaresInRow = 20;
     context.fillRect(0, 0, width, height);
-    if (width != height || width < 0) throw Error('Expected a square canvas.');
-    if (width % 20 != 0) throw Error('Expected the side of the canvas to be a multiple of 20.');
+    if (width !== height || width < 0) {
+        throw Error('Expected a square canvas.');
+    }
+    if (width % squaresInRow !== 0) {
+        throw Error('Expected the side of the canvas to be a multiple of 20.');
+    }
     var canvasSide = width;
-    var squareSide = width / 20;
+    var squareSide = width / squaresInRow;
     var tiles = [];
     for (var y = 0; y <= canvasSide - squareSide; y += squareSide) {
         var tileRow = [];
         for (var x = 0; x <= canvasSide - squareSide; x += squareSide) {
-            var tileValue = Math.random() * .5;
+            var tileValue = Math.random() * 0.5;
             tileRow.push(tileValue);
             context.fillStyle = colorBasedOnValue(tileValue);
             context.fillRect(x, y, squareSide, squareSide);
