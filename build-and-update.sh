15990e0fefae0e9ccb552f725d6bc7ac5bdfdd80
Bernardo Sulzbach <mafagafogigante@gmail.com>
2016-04-13 22:50:48 -0300

    The build and update script should be simpler
    Fixed #14

--- build-and-update.sh
+++ build-and-update.sh
@@ -1,7 +1,10 @@
 npm install
 gulp
-cp -r dist/* ../contagium.github.io/
-cd ../contagium.github.io
+git clone --depth 1 git@github.com:contagium/contagium.github.io.git
+cp -r dist/* contagium.github.io/
+cd contagium.github.io
 git add .
 git commit -am "Updated the website"
 git push
+cd ..
+rm -rf contagium.github.io
